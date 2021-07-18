3d2fa18fc3cccbb7f850e1d7ef3eeef6e4eabee2
jcdoncj <83451478+jcdoncj@users.noreply.github.com>
2021-07-18 09:58:47 -0700

    Retrieving title and post from database

--- index.php
+++ index.php
@@ -17,8 +17,8 @@
           
       
       <form class="com-mail">
-          <input disabled="" type="text" class="form-control1 control3"  value="<%=rs.getString("name")%>" name="title"  >
-          <textarea disabled="" rows="6" class="form-control1 control2"  id="disabledinput" name="message" > <%=rs.getString("post")%></textarea>
+          <input disabled="" type="text" class="form-control1 control3"  value="<?php echo $row['title']; ?> name="title"  >
+          <textarea disabled="" rows="6" class="form-control1 control2"  value="<?php echo $row['post']; ?> name="post"</textarea>
       </form>
     </ul>
 </div>
