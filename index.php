4f43befd35e5622e2f34b28fddfa0b9777e12f98
jcdoncj <83451478+jcdoncj@users.noreply.github.com>
2021-07-18 09:54:58 -0700

    At index page, post and comment will display.

--- index.php
+++ index.php
@@ -14,8 +14,12 @@
         <li><a href="login.php">login</a></li>
         <li><a href="Registration.php">Sign Up</a></li>
         <li><a href="google.com">Contact us</a></li>
-
-
+          
+      
+      <form class="com-mail">
+          <input disabled="" type="text" class="form-control1 control3"  value="<%=rs.getString("name")%>" name="title"  >
+          <textarea disabled="" rows="6" class="form-control1 control2"  id="disabledinput" name="message" > <%=rs.getString("post")%></textarea>
+      </form>
     </ul>
 </div>
 
