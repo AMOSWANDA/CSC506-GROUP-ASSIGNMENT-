4216f74dc78d39fb98374b5f3ba3a6a992f64720
NACHAMADA <84945843+NACHAMADA@users.noreply.github.com>
2021-07-18 18:04:53 +0100

    Admin _Login _Authen and Delete

---
+++ Admin_login_Authen.php
@@ -0,0 +1,136 @@
+<html>  
+<head lang="en">  
+    <meta charset="UTF-8">  
+    <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">  
+    <title>Admin Login</title>  
+</head>  
+<style>  
+    .login-panel {  
+        margin-top: 150px;  
+  
+</style>  
+  
+<body>  
+  
+<div class="container">  
+    <div class="row">  
+        <div class="col-md-4 col-md-offset-4">  
+            <div class="login-panel panel panel-success">  
+                <div class="panel-heading">  
+                    <h3 class="panel-title">Sign In</h3>  
+                </div>  
+                <div class="panel-body">  
+                    <form role="form" method="post" action="admin_login.php">  
+                        <fieldset>  
+                            <div class="form-group"  >  
+                                <input class="form-control" placeholder="Name" name="admin_name" type="text" autofocus>  
+                            </div>  
+                            <div class="form-group">  
+                                <input class="form-control" placeholder="Password" name="admin_pass" type="password" value="">  
+                            </div>  
+                            <input class="btn btn-lg btn-success btn-block" type="submit" value="login" name="admin_login" >  
+                        </fieldset>  
+                    </form>  
+                </div>  
+            </div>  
+        </div>  
+    </div>  
+</div>  
+</body>  
+  
+</html>  
+  
+<?php  
+/** 
+ * Created by PhpStorm. 
+ * User: Ehtesham Mehmood 
+ * Date: 11/24/2014 
+ * Time: 3:26 AM 
+ */  
+include("database/db_conection.php");  
+  
+if(isset($_POST['admin_login']))//this will tell us what to do if some data has been post through form with button.  
+{  
+    $admin_name=$_POST['admin_name'];  
+    $admin_pass=$_POST['admin_pass'];  
+  
+    $admin_query="select * from admin where admin_name='$admin_name' AND admin_pass='$admin_pass'";  
+  
+    $run_query=mysqli_query($dbcon,$admin_query);  
+  
+    if(mysqli_num_rows($run_query)>0)  
+    {  
+  
+        echo "<script>window.open('view_users.php','_self')</script>";  
+    }  
+    else {echo"<script>alert('Admin Details are incorrect..!')</script>";}  
+  
+}  
+  
+?>  
+View_users.php
+<html>  
+<head lang="en">  
+    <meta charset="UTF-8">  
+    <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css"> <!--css file link in bootstrap folder-->  
+    <title>View Users</title>  
+</head>  
+<style>  
+    .login-panel {  
+        margin-top: 150px;  
+    }  
+    .table {  
+        margin-top: 50px;  
+     }  
+</style>  
+  
+<body>  
+  
+<div class="table-scrol">  
+    <h1 align="center">All the Users</h1>  
+  
+<div class="table-responsive"><!--this is used for responsive display in mobile and other devices-->  
+    <table class="table table-bordered table-hover table-striped" style="table-layout: fixed">  
+        <thead>  
+  
+        <tr>  
+  
+            <th>User Id</th>  
+            <th>User Name</th>  
+            <th>User E-mail</th>  
+            <th>User Pass</th>  
+            <th>Delete User</th>  
+        </tr>  
+        </thead>  
+  
+        <?php  
+        include("database/db_conection.php");  
+        $view_users_query="select * from users";//select query for viewing users.  
+        $run=mysqli_query($dbcon,$view_users_query);//here run the sql query.  
+  
+        while($row=mysqli_fetch_array($run))//while look to fetch the result and store in a array $row.  
+        {  
+            $user_id=$row[0];  
+            $user_name=$row[1];  
+            $user_email=$row[2];  
+            $user_pass=$row[3];  
+  
+        ?>  
+  
+        <tr>  
+<!--here showing results in the table -->  
+            <td><?php echo $user_id;  ?></td>  
+            <td><?php echo $user_name;  ?></td>  
+            <td><?php echo $user_email;  ?></td>  
+            <td><?php echo $user_pass;  ?></td>  
+            <td><a href="delete.php?del=<?php echo $user_id ?>"><button class="btn btn-danger">Delete</button></a></td> <!--btn btn-danger is a bootstrap button to show danger-->  
+        </tr>  
+  
+        <?php } ?>  
+  
+    </table>  
+        </div>  
+</div>  
+</body>  
+  
+</html>  
